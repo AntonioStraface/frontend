86294f32fefa7b73c2d874ee14eb30f26cb5a846
NOTB726\admin <gruppoclassees@gmail.com>
2018-02-07 12:56:50 +0100

    somma e differenza per scelta di boolean

---
+++ javascript/js/main.js
@@ -0,0 +1,3 @@
+var x = [4,5,6];
+console.log(sceltaoperazione(true,x));
+console.log(sceltaoperazione(false,x));
---
+++ javascript/js/utils.js
@@ -0,0 +1,28 @@
+//somma
+var sum = function  (array){
+  var totsum = 0;
+  for(var i = 0; i < array.length; i ++){
+    totsum += array[i];
+  }
+  return totsum;
+}
+
+//differenza
+var dif = function  (array){
+  var totdif = array[0];
+  for(var i = 1; i < array.length; i ++){
+    totdif -= array[i];
+  }
+  return totdif;
+}
+
+var sceltaoperazione = function (scelta,array){
+  var  risultato = 0 ;
+  if(scelta == true){
+   risultato = sum(array);
+  }
+  else {
+     risultato = dif(array);
+  }
+  return risultato ;
+}
