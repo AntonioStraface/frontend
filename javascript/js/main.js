4628f73649cf08b1f94f2c3a171d2062f13bbfbb
NOTB726\admin <gruppoclassees@gmail.com>
2018-02-07 14:23:20 +0100

    NO regex, deve tornare a casa

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -2,3 +2,4 @@ var x = [4,5,6];
 console.log(sceltaoperazione(true,x));
 console.log(sceltaoperazione(false,x));
 console.log(removeParola("ciao sono MasterRegex ciao \n ciao a tutti", "ciao"));
+console.log(eliminaParola("ciao sono anti-Regex ciao ciao a tutti", "ciao"));
--- javascript/js/utils.js
+++ javascript/js/utils.js
@@ -34,3 +34,17 @@ var sceltaoperazione = function (scelta,array){
 var removeParola = function(frase, parola){
   return frase.replace(new RegExp(parola, "igm"),"");
 }
+
+//Versione migliore
+var eliminaParola = function(frase, parola) {
+  var temp = frase.split(" ");
+
+  for (var i = 0; i < temp.length; i++) {
+    if (temp[i]===parola) {
+      temp = temp.slice(0,i).concat(temp.slice(i+1));
+      i--;
+    }
+  }
+
+    return temp.join(" ");
+}
