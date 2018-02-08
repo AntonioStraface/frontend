ea820ff84a16fc30cd9fde00b3f6c70556829bb6
NOTB726\admin <gruppoclassees@gmail.com>
2018-02-08 13:01:49 +0100

    cariola

--- MattiMattoni/js/function.js
+++ MattiMattoni/js/function.js
@@ -1,13 +1,22 @@
 //MattiMattoni
 
+//caching
 var $creazione = $(".creatore");
 var $prototipo = $("<div></div>").addClass("mattone");
+var $distruzione = $(".distruttore");
+var $cariola = $(".cariola");
 
+//Logic
 var crea = function(){
-var $clone = $prototipo.clone();
-$(".cariola").append($clone);
-
+  var $clone = $prototipo.clone();
+  $cariola.append($clone);
 }
 
+var distruzioneTotale= function(){
+  $(".mattone").remove();
+};
 
+
+//handler
+$distruzione.on("click", distruzioneTotale);
 $creazione.on("click",crea);
