524a61881c71f4f79c929e4eb6a9507910765b27
NOTB726\admin <gruppoclassees@gmail.com>
2018-02-12 17:55:40 +0100

    Buttons with click events to show images thumbnails

--- paginatore/js/module.js
+++ paginatore/js/module.js
@@ -1,12 +1,12 @@
 var getPaginatore = (function () {
 
   /* DECLARING VARIABLES */
-  var $wrapper,$showMore,
-      $itemTemplate = $('<li></li>').addClass('element list-group-item text-success nascondi'),
-      RESTURL = "https://jsonplaceholder.typicode.com/photos";
-      var counter = 1;
-      var mostrati = 50;
-      var MOSTRATIINCR = 500;
+  var $wrapper, $showMore,
+  $itemTemplate = $('<li><span></span><button>Show more &raquo;</button></li>').addClass('element list-group-item text-success nascondi'),
+  counter = 1, mostrati = 50;
+
+  var MOSTRATIINCR = 50,
+  RESTURL = "https://jsonplaceholder.typicode.com/photos";
 
   /* CACHING VARIABLES */
   function _setup() {
@@ -14,32 +14,42 @@ var getPaginatore = (function () {
     $showMore = $(".show-more");
   };
 
-   /* PRIVATE BUSINESS FUNCTIONS */
-  var _aggiungere = function(attributo,tipo,$appendTO) {
-    var $createEl = $itemTemplate.attr("data-" + tipo + "-type",attributo).clone();
-    $itemTemplate.attr("data-num",counter);
-    $createEl.text(attributo);
+  /* PRIVATE BUSINESS FUNCTIONS */
+  var _aggiungere = function(item, $appendTO) {
+    var $createEl = $itemTemplate.attr("data-title-type", item.title).clone();
+    $createEl.attr("data-id-type", item.id);
+    $createEl.attr("data-num", counter);
+    $createEl.find('SPAN').text(item.title);
+
     $appendTO.append($createEl);
     counter++;
   };
 
-var _mostra = function (){
-  for (var i = 1; i <= mostrati; i++) {
-    $("li[data-num="+i+"]").removeClass("nascondi");
+  var _mostra = function (){
+    for (var i = 1; i <= mostrati; i++) {
+      $("li[data-num="+i+"]").removeClass("nascondi");
+    }
+    mostrati += MOSTRATIINCR;
   }
-  mostrati += MOSTRATIINCR;
-}
- var _filteringData = function(data) {
+
+  var _filteringData = function(data) {
     $wrapper.find('.list-group').empty();
-    debugger;
+
     data.forEach(function(element){
-      _aggiungere(element.title,"title",$wrapper.find('.list-group'));
+      _aggiungere(element, $wrapper.find('.list-group'));
     });
     _mostra();
- }
+  }
+
+  var _addSingleItem = function(element, $itsWrapper) {
+
+    $itsWrapper.append(
+      $('<img src="'+element[0].thumbnailUrl+'" />')
+    );
+  }
 
   var _getFoto = function() {
-    debugger;
+
     var url = RESTURL;
     $.ajax({
       url: url,
@@ -47,19 +57,44 @@ var _mostra = function (){
       dataType: 'json',
       cache: false,
       success: function(data) {
-           _filteringData(data)
+        _filteringData(data)
       }
     });
   }
 
+  var _getItemData = function(itemId, $localWrapper) {
 
+    var url = RESTURL;
+
+    $.ajax({
+      url: url,
+      type: "GET",
+      dataType: 'json',
+      data: {
+        id: itemId
+      },
+      cache: false,
+      success: function(data) {
+        _addSingleItem(data, $localWrapper)
+      }
+    });
+  }
   /* END PRIVATE BUSINESS FUNCTIONS */
 
 
-   /* DECLARING EVENT HANDLER */
+  /* DECLARING EVENT HANDLER */
   function _setObserver() {
     _getFoto();
     $showMore.on("click",_mostra);
+
+    $wrapper.on('click', 'BUTTON', function(){
+
+      var $this = $(this);
+
+      var id = $this.closest('LI').attr('data-id-type');
+
+      _getItemData(id, $this.closest('LI'));
+    });
   };
 
   function _init() {
@@ -68,8 +103,8 @@ var _mostra = function (){
       _setObserver();
     }
     catch(e) {
-        console.log('%c ' + e.message, 'color:red');
-        console.log('%c ' + e.stack, 'background: #222; color: #bada55');
+      console.log('%c ' + e.message, 'color:red');
+      console.log('%c ' + e.stack, 'background: #222; color: #bada55');
     }
   }
 
