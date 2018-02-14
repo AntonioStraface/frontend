6063ae3b06ca1c1fe5c7e25fd2cbfac79a50f409
NOTB726\admin <gruppoclassees@gmail.com>
2018-02-14 10:19:57 +0100

    Paginatore Menu Birre

---
+++ Birra/js/main.js
@@ -0,0 +1,1 @@
+getProdotto.start();
---
+++ Birra/js/module.js
@@ -0,0 +1,84 @@
+var getProdotto = (function () {
+
+  /* DECLARING VARIABLES */
+  var RESTURL = "https://api.punkapi.com/v2/beers", ELEMENTI=4;
+  var pagina = 1;
+  var $listaProdotti, $creoProd, $sceltaProdotto, $birraInserita;
+  /* CACHING VARIABLES */
+  function _setup() {
+    $listaProdotti = $('.prodotti');
+    $sceltaProdotto = $('.sceltaProdotto');
+    $birraInserita = $('.birraInserita');
+    };
+
+   /* PRIVATE BUSINESS FUNCTIONS */
+  var _aggiuntaProdotto = function(element) {
+    $creoProd = $('<li> <img src=" "></img> <h3></h3> </li>').addClass('singoloProdotto');
+    var $prodotto = $creoProd.clone();
+    $prodotto.find('IMG').attr("src", element.image_url);
+    $prodotto.find('H3').text(element.name);
+    $listaProdotti.append($prodotto);
+  };
+
+  var _filteringData= function(data){
+    data.forEach(function(element){
+      _aggiuntaProdotto(element);
+    });
+  }
+
+  var _diversiMenu = function(){
+    debugger;
+    var inputBir = $birraInserita.val();
+      if(inputBir === ""){
+        _getPaginatore();
+      };
+  }
+
+  var _getPaginatore = function() {
+    var url=RESTURL;
+    $.ajax({
+      url:url,
+      type:"GET",
+      dataType:'json',
+      cache:false,
+      data:{
+        page:pagina,
+        per_page: ELEMENTI
+      },
+      success: function(data){
+        _filteringData(data)
+      }
+    });
+  }
+
+  var _bloccoInvio = function(e){
+    e.preventDefault();
+  }
+  /* END PRIVATE BUSINESS FUNCTIONS */
+
+   /* DECLARING EVENT HANDLER */
+  function _setObserver() {
+    $sceltaProdotto.on('submit', function(e){
+        _bloccoInvio(e);
+        _diversiMenu();
+
+    }
+
+  )};
+
+  function _init() {
+    try {
+      _setup();
+      _setObserver();
+    }
+    catch(e) {
+        console.log('%c ' + e.message, 'color:red');
+        console.log('%c ' + e.stack, 'background: #222; color: #bada55');
+    }
+  }
+
+  return {
+    start: _init
+  };
+
+})();
